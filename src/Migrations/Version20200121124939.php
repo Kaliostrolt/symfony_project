5aae7330d2c55ab325243a8ab1d8b0c63ae618ba
erfolen <dlababy@mail.ru>
2020-01-21 21:51:20 +0300

    admin role, manager role, user role

--- src/Migrations/Version20200121124939.php
+++ src/Migrations/Version20200121124939.php
@@ -6,12 +6,19 @@ namespace DoctrineMigrations;
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
+use Ramsey\Uuid\Uuid;
 
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
 final class Version20200121124939 extends AbstractMigration
 {
+    private $roles = [
+        "admin" => "Admin",
+        "manager" => "Manager",
+        "user" => "User"
+    ];
+
     public function getDescription() : string
     {
         return '';
@@ -20,7 +27,12 @@ final class Version20200121124939 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+            foreach ($this->roles as $key => $value){
+                $id = Uuid::uuid4();
+                $this->addSql("INSERT INTO role (id, name, label, creaded_at, creaded_by) VALUES ('$id','$key', '$value', current_date, '66aca6ab-8b58-4d14-8294-af9857877d4a')");
+        }
     }
 
     public function down(Schema $schema) : void
