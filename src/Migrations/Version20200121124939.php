89aee2cbde34473e1c73062a2a36ff7138cb11fc
erfolen <dlababy@mail.ru>
2020-01-22 14:11:29 +0300

    add function down

--- src/Migrations/Version20200121124939.php
+++ src/Migrations/Version20200121124939.php
@@ -29,15 +29,18 @@ final class Version20200121124939 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-            foreach ($this->roles as $key => $value){
+        foreach ($this->roles as $key => $value) {
                 $id = Uuid::uuid4();
-                $this->addSql("INSERT INTO role (id, name, label, creaded_at, creaded_by) VALUES ('$id','$key', '$value', current_date, '66aca6ab-8b58-4d14-8294-af9857877d4a')");
+                $this->addSql("INSERT INTO role (id, name, label, creaded_at, creaded_by) VALUES ('$id','$key', '$value', now(), '66aca6ab-8b58-4d14-8294-af9857877d4a')");
         }
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $keyRoles=implode("', '", array_keys($this->roles));
+        $this->addSql("DELETE FROM role WHERE name IN('$keyRoles')");
 
     }
 }
